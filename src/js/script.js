5c29ad65995ed5f1aebbc2fe73f22a87ca013a77
Vladislav Petrov <=>
2021-10-20 14:29:38 +0400

    refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,22 @@ import * as zip from '../../node_modules/@zip.js/zip.js/lib/zip.js';
 class DOCXParser {
     constructor(file) {
         this.file = file;
+
+        // Общая структура для всех шаблонов
+        this.resultData = {
+            code: null,
+            type: null,
+            subtype: null,
+            tables: []
+        };
     }
 
+    // Парсим выбранный файл
     async parseDOCX() {
         const reader = new zip.ZipReader(new zip.BlobReader(this.file));
         const entries = await reader.getEntries();
         let parsedText;
-    
+
         for (let key in entries) {
             const entry = entries[key];
     
@@ -20,7 +29,7 @@ class DOCXParser {
                 break;
             }
         }
-    
+
         const parser = new DOMParser();
         const XMLDocument = parser.parseFromString(parsedText, 'application/xml');
 
@@ -33,81 +42,88 @@ class DOCXParser {
 
     }
 
+    // Парсим DOM
     _parseDOM() {
-        // Объект с возвращаемым контентом
-        const resultContent = {
-            code: null,
-            type: null,
-            subtype: null,
-            tables: []
-        };
-
         // Массив с контентом документа
         const documentContent = Array.from(this.XMLDocument.querySelector('document').querySelector('body').childNodes);
         console.log(documentContent);
 
         // Проходимся по узлам контента документа и выбираем то, что необходимо
         const newResult = documentContent.map((node) => {
-            return this._parseNodeContent(node, resultContent);
+            return this._parseNodeContent(node);
         });
 
         console.log(newResult);
     }
 
-    _parseNodeContent(node, resultContent = null) {
+    // Парсим контент отдельного узла из DOM
+    _parseNodeContent(node) {
         switch (node.nodeName.slice(2)) {
             // Если узел - параграф
             case 'p':
                 // Берем строки параграфа
                 const rowsArray = Array.from(node.querySelectorAll('r'));
-    
+
                 // Проходимся по полученным строкам
-                return rowsArray.map((row) => {
+                const parsedRows = rowsArray.map((row) => {
                     // Получаем массив с контентом из строки
                     const rowContentArray = Array.from(row.childNodes);
-    
+
                     // Проходимся по массиву с контентом из строки, предварительно отфильтровав нужный тип контента
-                    return rowContentArray.filter((node) => {
+                    const parsedRowContent = rowContentArray.filter((node) => {
                         return node.nodeName.slice(2) === 't';
                     }).map((node) => {
                         // Если текущий узел - текст
                         if (node.nodeName.slice(2) === 't') {
                             // Считываем текст узла
                             const nodeText = node.textContent;
-    
+
                             // Если считанный текст содержит код ЭОМа
                             if (nodeText.includes('Код ЭОМа:')) {
-                                resultContent.code = nodeText.slice(9).trim();
+                                this.resultData.code = nodeText.slice(9).trim();
                             }
-    
+
                             // Если считанный текст содержит тип и подтип
                             if (nodeText.includes('::')) {
                                 const index = nodeText.indexOf('::');
-    
-                                resultContent.type = nodeText.slice(0, index).trim();
-                                resultContent.subtype = nodeText.slice(index + 2).trim();
+
+                                this.resultData.type = nodeText.slice(0, index).trim();
+                                this.resultData.subtype = nodeText.slice(index + 2).trim();
                             }
-    
-                            return nodeText;
+
+                            return {
+                                type: 'text',
+                                content: nodeText
+                            };
                         }
                     });
+
+                    return {
+                        type: 'paragraphRow',
+                        content: parsedRowContent
+                    };
                 });
+
+                return {
+                    type: 'paragraph',
+                    rows: parsedRows
+                };
             // Если узел - таблица
             case 'tbl':
                 // Выбираем только строки
                 const tableRows = Array.from(node.childNodes).filter((node) => {
                     return node.nodeName.slice(2) === 'tr';
                 });
-    
+
                 // Проходимся по строкам таблицы
-                return tableRows.map((row) => {
+                const parsedTableRows = tableRows.map((row) => {
                     // В строке выбираем только ячейки
                     const rowCells = Array.from(row.childNodes).filter((node) => {
                         return node.nodeName.slice(2) === 'tc';
                     });
-    
+
                     // Проходимся по ячейкам одной строки
-                    return rowCells.map((cell) => {
+                    const parsedTableCells = rowCells.map((cell) => {
                         // Проходимся по контенту одной ячейки
                         return Array.from(cell.childNodes).filter((node) => {
                             return (
@@ -118,7 +134,17 @@ class DOCXParser {
                             return this._parseNodeContent(node);
                         });
                     });
+
+                    return {
+                        type: 'tableRow',
+                        cells: parsedTableCells
+                    }
                 });
+
+                return {
+                    type: 'table',
+                    rows: parsedTableRows
+                };
             default:
                 break;
         }
@@ -132,41 +158,13 @@ const init = function() {
         if (!fileInput.files.length) return;
 
         const selectedFile = fileInput.files[0];
+
+        // Создаем объект нашего парсера
         const parserObject = new DOCXParser(selectedFile);
 
+        // Парсим выбранный файл
         await parserObject.parseDOCX();
     });
 }
 
 init();
-
-
-
-
-
-
-// const getBlobFromImage = function(imageBlob) {
-//     return new Promise((resolve) => {
-//         const imageURL = URL.createObjectURL(imageBlob);
-//         let image = new Image();
-
-//         image.onload = () => {
-//             URL.revokeObjectURL(imageURL);
-//             resolve(image);
-//         }
-
-//         image.src = imageURL;
-//     });
-// }
-
-// const getBase64 = function(image, type) {
-//     const canvasElement = document.createElement('canvas');
-//     const context = canvasElement.getContext('2d');
-
-//     canvasElement.width = image.width;
-//     canvasElement.height = image.height;
-
-//     context.drawImage(image, 0, 0);
-
-//     return canvasElement.toDataURL(`image/${type}`);
-// }
